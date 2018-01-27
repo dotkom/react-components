00f8e5f4f543d341efcbe9e1cc1cfa1fa4a44f2b
Håkon Solbjørg <hlsolbjorg@gmail.com>
2018-01-28 00:14:16 +0100

    Rewrite Button to pure function

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -3,33 +3,18 @@ import PropTypes from 'prop-types';
 
 import _s from './Button.css';
 
-class Button extends React.Component {
-  _mouseEnter(...a) {
-    if(this.props.onMouseEnter)
-      this.props.onMouseEnter(...a);
-  }
-
-  _mouseLeave(...a) {
-    if(this.props.onMouseLeave)
-      this.props.onMouseLeave(...a);
-  }
-
-  render() {
-    const { children, ...rest } = this.props;
-    
-    return (
-      <button 
-        {...rest}
-        className={_s.button} 
-        onMouseEnter={(...a) => this._mouseEnter(...a)}
-        onMouseLeave={(...a) => this._mouseLeave(...a)}
-      >
-        <div className={_s.buttonContent}>
-          { children }
-        </div>
-      </button>
-    );
-  }
+function Button(props) {
+  const { children, ...rest } = props;
+  return (
+    <button
+      {...rest}
+      className={_s.button}
+    >
+      <div className={_s.buttonContent}>
+        { children }
+      </div>
+    </button>
+  );
 }
 
 Button.propTypes = {
