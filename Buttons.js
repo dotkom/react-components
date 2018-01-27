a7cf4f4af5fb5f1b48eaf8bc73d672652f4713cf
Håkon Solbjørg <hlsolbjorg@gmail.com>
2018-01-27 22:44:47 +0100

    Add index.js as main module file; export components

---
+++ Buttons.js
@@ -0,0 +1,3 @@
+export { default as Button } from './src/components/Button/Button';
+export { default as IconButton } from './src/components/Button/IconButton';
+export { default as LoginButton } from './src/components/Button/LoginButton';
